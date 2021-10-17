e92767b658c5f26590f0dc3903af782934b14c98
Peter Blanchfield <zygomy@gmail.com>
2021-10-17 15:12:06 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDoc
+{
+    public partial class Form1 : Form
+    {
+        MyNewClass mnc;
+        /// <summary>
+        /// Initialise test MyNewClass as size 4
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            mnc = new MyNewClass(4);
+        }
+        /// <summary>
+        /// add test data to MyNewClass.
+        /// First test in bounds and extreme data 
+        /// then out of bounds data
+        /// Test passes when correct messages returned for
+        /// Adding data
+        /// And retreiving data
+        /// </summary>
+        /// <param name="sender">Loader object</param>
+        /// <param name="e">Loading event</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(float f = 0; f < 6; f++)
+            {
+                int response = mnc.AddData(f);
+                if (response >= 0)
+                {
+                    listBox1.Items.Add(f + " added to array.");
+                }
+                else
+                {
+                    listBox1.Items.Add(f + " could not be added - array full!");
+                }
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                float f = mnc.DataAt(i);
+                if(!(f.Equals(float.NaN)))
+                {
+                    listBox1.Items.Add("Number at location "+ i + " is " + f);
+                }
+                else
+                    listBox1.Items.Add(i + " is beyond the array boundary!");
+            }
+        }
+    }
+}
---
+++ MyNewClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDoc
+{
+    /// <summary>
+    /// class to test documenting
+    /// A class to store an array for statistics
+    /// </summary>
+    /// 
+
+    class MyNewClass
+    {
+        /// <summary>
+        /// n the size of the array is passed to the constructor
+        /// and stored in the parameter desired array size
+        /// </summary>
+        int desiredArraySize;
+        /// <summary>
+        /// contentSize is how much data is currently stored
+        /// </summary>
+        int contentSize;
+        float[] arrayData;
+
+        /// <summary>
+        /// class consturctor
+        /// </summary>
+        /// <param name="n">The size of the array</param>
+        public MyNewClass(int n)
+        {
+            desiredArraySize = n;
+            arrayData = new float[n];
+            contentSize = 0;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="f">The data to be added</param>
+        /// <returns>
+        /// 0 if worked
+        /// -1 if failed
+        /// </returns>
+        public int AddData(float f)
+        {
+            if(contentSize < desiredArraySize)
+            {
+                arrayData[contentSize++] = f;
+
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        /// <summary>
+        /// A function to add data to the class
+        /// </summary>
+        /// <param name="n">The item in the array</param>
+        /// <returns>
+        /// arrayData[n] if n <contentSize> 
+        /// NaN if n > </contentSize>
+        /// </returns>
+        public float DataAt(int n)
+        {
+            if(n < contentSize)
+            {
+                return arrayData[n];
+            }
+            else
+            {
+                return float.NaN;
+            }
+
+        }
+
+    }
+}
